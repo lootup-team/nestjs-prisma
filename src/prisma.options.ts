c1c75decc946ac10772d4c8aa440302cc6a997bf
DestyK <nikita.karpov.1910@mail.ru>
2025-07-01 02:10:38 +0800

    feat: add `CreatePrismaOptionsFactory` update`ConfigurableModuleBuilder`
    This commit introduces the `CreatePrismaOptionsFactory` interface, which
    defines a contract for classes responsible for creating Prisma module
    options. This improves code organization, testability, and promotes
    dependency injection.
    
    The commit also updates the `ConfigurableModuleBuilder` to utilize this
    interface. Specifically, the `.setFactoryMethodName('createPrismaOptions')`
    is added to properly configure the builder to use the
    `createPrismaOptions` method of classes implementing the
    `CreatePrismaOptionsFactory` interface.

--- src/prisma.options.ts
+++ src/prisma.options.ts
@@ -8,7 +8,14 @@ export interface PrismaModuleOptions<T extends PrismaClientType> {
   customService?: Type<PrismaService<T>>;
 }
 
+export interface CreatePrismaOptionsFactory<T extends PrismaClientType> {
+  createPrismaOptions():
+    | PrismaModuleOptions<T>
+    | Promise<PrismaModuleOptions<T>>;
+}
+
 export const { ConfigurableModuleClass, MODULE_OPTIONS_TOKEN } =
   new ConfigurableModuleBuilder<PrismaModuleOptions<PrismaClientType>>()
     .setClassMethodName('forRoot')
+    .setFactoryMethodName('createPrismaOptions')
     .build();
