24340b21176a1267d1a10a0c88657f1e4eb57967
DestyK <nikita.karpov.1910@mail.ru>
2025-07-01 02:12:59 +0800

    fix: add export `CreatePrismaOptionsFactory` to index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 export * from './prisma.decorator';
 export * from './prisma.module';
 export * from './prisma.service';
-export { PrismaModuleOptions, MODULE_OPTIONS_TOKEN } from './prisma.options';
+export {
+  PrismaModuleOptions,
+  CreatePrismaOptionsFactory,
+  MODULE_OPTIONS_TOKEN,
+} from './prisma.options';
